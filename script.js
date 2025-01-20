ea0b9d6875b1e77cec688712508ae7228df74b58
Daniel <rasay_d@yahoo.com>
2025-01-20 10:22:31 +0800

    Create logic func for player pick

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // FIXME: - Setup file folder in Github with index.html, readme, js ScriptProcessorNode
 // - Create function that will make the AI pick R,P,S {via Math.floor(Math.random())}
-// - Create function that prompts user to pick R,P,S
+// - Create function that prompts user to pick R,P,S using getHumanChoice()
 // - Initialize a '0' counter for player and computer scores 
 // - Create a function that takes the player and comp variables as argument to start a round (playRound)
         // - IF player(rock) vs. comp(scissors)
@@ -11,9 +11,9 @@
             //  - RETURN: increment 1 computer point and log "You lose! y beats x."
 // - Create a function that calls 5 rounds
 
-let rck = "Rock";
-let ppr = "Paper";
-let scr = "Scissors";
+let rck = "rock";
+let ppr = "paper";
+let scr = "scissors";
 
 function getComputerChoice() {
     var i = Math.floor((Math.random()*3) +1);
@@ -28,7 +28,16 @@ function getComputerChoice() {
     if (i == 3) {return scr};
 };
 
+function getHumanChoice() {
+    let pick = prompt (`What\'s your pick? `);
+    // research how to make the string variables case-insensitive
+    // SOlution: trim and convert to lowercase before comparison
+    if (pick == undefined) {alert("What the fudge??");}
 
+    pick = pick.trim().toLowerCase();
 
+    if (pick == "rock" || pick == "paper" || pick == "scissors") {return pick;}
+    else {alert("Hey, please choose rock, paper, or scissors!");}
+    };
 
 console.log("Hello Odin!")
